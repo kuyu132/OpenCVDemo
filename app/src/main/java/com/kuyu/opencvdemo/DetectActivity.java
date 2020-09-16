26faa9b24b405c8309a129c037b6654a2f123d05
wangguoqun <wangguoqun@37.com>
2020-09-16 21:16:04 +0800

    init

---
+++ app/src/main/java/com/kuyu/opencvdemo/DetectActivity.java
@@ -0,0 +1,123 @@
+package com.kuyu.opencvdemo;
+
+import android.content.Context;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+public class DetectActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    // 手动装载openCV库文件，以保证手机无需安装OpenCV Manager
+    static {
+        System.loadLibrary("opencv_java4");
+    }
+
+    private JavaCameraView mCameraView;
+    private Mat mGray;
+    private Mat mRgba;
+    private int mAbsoluteFaceSize = 0;
+    private boolean isFrontCamera =false;
+    private CascadeClassifier classifier;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detect);
+
+        initClassifier();
+        initViews();
+    }
+
+    private void initViews(){
+        mCameraView = findViewById(R.id.camera_view);
+        mCameraView.setCvCameraViewListener(this);
+        mCameraView.setCameraIndex(CameraBridgeViewBase.CAMERA_ID_BACK);
+        mCameraView.setCameraPermissionGranted();
+        mCameraView.enableView();
+    }
+
+    @Override public void onCameraViewStarted(int width, int height) {
+        mGray = new Mat();
+        mRgba = new Mat();
+    }
+
+    @Override public void onCameraViewStopped() {
+        mGray.release();
+        mRgba.release();
+    }
+
+    // 初始化人脸级联分类器，必须先初始化
+    private void initClassifier() {
+        try {
+            InputStream is = getResources().openRawResource(R.raw.lbpcascade_frontalface);
+            File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+            File cascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
+            FileOutputStream os = new FileOutputStream(cascadeFile);
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+            classifier = new CascadeClassifier(cascadeFile.getAbsolutePath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        mGray = inputFrame.gray();
+        // 翻转矩阵以适配前后置摄像头
+        if (isFrontCamera) {
+            Core.flip(mRgba, mRgba, 1);
+            Core.flip(mGray, mGray, 1);
+        } else {
+            Core.flip(mRgba, mRgba, -1);
+            Core.flip(mGray, mGray, -1);
+        }
+        float mRelativeFaceSize = 0.2f;
+        if (mAbsoluteFaceSize == 0) {
+            int height = mGray.rows();
+            if (Math.round(height * mRelativeFaceSize) > 0) {
+                mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
+            }
+        }
+        MatOfRect faces = new MatOfRect();
+        if (classifier != null)
+            classifier.detectMultiScale(mGray, faces, 1.1, 2, 2,
+                    new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
+        Rect[] facesArray = faces.toArray();
+        Scalar faceRectColor = new Scalar(0, 255, 0, 255);
+        for (Rect faceRect : facesArray)
+            Imgproc.rectangle(mRgba, faceRect.tl(), faceRect.br(), faceRectColor, 3);
+        return mRgba;
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mCameraView != null) {
+            mCameraView.disableView();
+        }
+    }
+
+    @Override protected void onDestroy() {
+        super.onDestroy();
+        mCameraView.disableView();
+    }
+}
